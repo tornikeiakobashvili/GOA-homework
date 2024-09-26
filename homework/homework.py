9550f9659155a1a5dd518e96fa960c0637f59784
tornikeiakobashvili <tornikeiakobashvili901@gmail.com>
2024-09-26 17:23:39 +0400

    second commit

---
+++ homework/homework.py
@@ -0,0 +1,16 @@
+
+
+mother = "სოფო"
+
+are = "არის"
+
+my_mother = "დედა ჩმი"
+
+father = "გიორგი"
+
+my_father = "მამა ჩემი"
+
+sister = "მარიამი"
+
+my_sister = "ჩემი და"
+print(mother + " " + are  + " " + my_mother + " " + father + " " + are + " " + my_father + " " + sister + " " + are + " " + my_sister)
